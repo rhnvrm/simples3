e4c7d93da1b3d6c355c1cc6cc3eba4be22710497
Rohan Verma <hello@rohanverma.net>
2018-12-24 17:12:20 +0530

    Update readme and docstrings, minor refactors.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview [![GoDoc](https://godoc.org/github.com/rhnvrm/simples3?status.svg)](https://godoc.org/github.com/rhnvrm/simples3) [![Go Report Card](https://goreportcard.com/badge/github.com/rhnvrm/simples3)](https://goreportcard.com/report/github.com/rhnvrm/simples3)
 
-SimpleS3 is a golang library for uploading and deleting objects on S3 buckets using the REST API calls signed using AWS Signature Version 4
+SimpleS3 is a golang library for uploading and deleting objects on S3 buckets using REST API calls or Presigned URLs signed using AWS Signature Version 4.
 
 ## Install
 
@@ -16,7 +16,16 @@ go get github.com/rhnvrm/simples3
 testTxt, _ := os.Open("test.txt")
 defer testTxt.Close()
 
+// Create an instance of the package
+// You can either create by manually supplying credentials
+// (preferably using Environment vars)
 s3 := simples3.New(Region, AWSAccessKey, AWSSecretKey)
+// or you can use this on an EC2 instance to 
+// obtain credentials from IAM attached to the instance.
+s3 := simples3.NewUsingIAM(Region)
+
+// File Upload is as simple as providing the following
+// details.
 err := s3.FileUpload(simples3.UploadInput{
     Bucket:      AWSBucket,
     ObjectKey:   "test.txt",
@@ -24,19 +33,34 @@ err := s3.FileUpload(simples3.UploadInput{
     FileName:    "test.txt",
     Body:        testTxt,
 }
+
+// Similarly, Files can be deleted.
+err := s3.FileDelete(simples3.DeleteInput{
+    Bucket:    os.Getenv("AWS_S3_BUCKET"),
+    ObjectKey: "test.txt",
+}
+
+// You can also use this library to generate
+// Presigned URLs that can for eg. be used to
+// GET/PUT files on S3 through the browser.
+var time, _ = time.Parse(time.RFC1123, "Fri, 24 May 2013 00:00:00 GMT")
+
+url := s.GeneratePresignedURL(PresignedInput{
+    Bucket:        "examplebucket",
+    ObjectKey:     "test.txt",
+    Method:        "GET",
+    Timestamp:     time,
+    ExpirySeconds: 86400,
+})
 ```
 
 ## Contributing
 
-ToDo.
-
-## Contributors
-
-ToDo.
+You are more than welcome to contribute to this project. Fork and make a Pull Request, or create an Issue if you see any problem or want to propose a feature.
 
 ## Author
 
-ToDo.
+Rohan Verma <hello@rohanverma.net>
 
 ## License
 
--- presigned.go
+++ presigned.go
@@ -16,6 +16,7 @@ const (
 	defaultPresignedURLFormat = "%s.s3.amazonaws.com" // <bucket>
 )
 
+// PresignedInput is passed to GeneratePresignedURL as a parameter.
 type PresignedInput struct {
 	Bucket        string
 	ObjectKey     string
@@ -25,10 +26,13 @@ type PresignedInput struct {
 	ExpirySeconds int
 }
 
+// GeneratePresignedURL creates a Presigned URL that can be used
+// for Authentication using Query Parameters.
+// (https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html)
 func (s3 *S3) GeneratePresignedURL(in PresignedInput) string {
 	var (
 		nowTime = in.Timestamp.UTC()
-		cred    = s3.buildCredential(nowTime)
+		cred    = fmt.Sprintf("%s/"+s3.buildCredentialWithoutKey(nowTime), s3.AccessKey)
 		amzdate = nowTime.Format(amzDateISO8601TimeFormat)
 		expiry  = strconv.Itoa(in.ExpirySeconds)
 	)
@@ -41,7 +45,7 @@ func (s3 *S3) GeneratePresignedURL(in PresignedInput) string {
 	signedHeaders["host"] = fmt.Sprintf(defaultPresignedURLFormat, in.Bucket)
 
 	// Start Canonical Request Formation
-	h := sha256.New()
+	h := sha256.New()                     // We write the canonical request directly to the SHA256 hash.
 	fmt.Fprintf(h, "%s\n", in.Method)     // HTTP Verb
 	fmt.Fprintf(h, "/%s\n", in.ObjectKey) // CanonicalURL
 
@@ -54,44 +58,53 @@ func (s3 *S3) GeneratePresignedURL(in PresignedInput) string {
 	}
 
 	// We need to have a sorted order,
-	keysQS := make([]string, 0, len(queryString))
+	// for QueryStrings and SignedHeaders
+	sortedQS := make([]string, 0, len(queryString))
 	for name := range queryString {
-		keysQS = append(keysQS, name)
+		sortedQS = append(sortedQS, name)
 	}
-	sort.Strings(keysQS) //sort by key
-	for _, k := range keysQS {
+	sort.Strings(sortedQS) //sort by key
+
+	sortedSH := make([]string, 0, len(signedHeaders))
+	for name := range signedHeaders {
+		sortedSH = append(sortedSH, name)
+	}
+	sort.Strings(sortedSH) //sort by key
+
+	// Proceed to write canonical query params
+	for _, k := range sortedQS {
 		fmt.Fprintf(h, "%s=%s&", url.QueryEscape(k), url.QueryEscape(queryString[k])) // HTTP Verb
 	}
 
 	fmt.Fprintf(h, "X-Amz-SignedHeaders=")
 	// Add Signed Headers to Query String
 	first := true
-	for k := range signedHeaders {
+	for k := range sortedSH {
 		if first {
-			fmt.Fprintf(h, "%s", url.QueryEscape(k))
+			fmt.Fprintf(h, "%s", url.QueryEscape(sortedSH[k]))
 			first = false
 		} else {
-			fmt.Fprintf(h, "%s", url.QueryEscape(";"+k))
+			fmt.Fprintf(h, "%s", url.QueryEscape(";"+sortedSH[k]))
 		}
 	}
 	fmt.Fprintf(h, "\n")
 	// End QueryString Params
 
 	// Start Canonical Headers
-	for k, v := range signedHeaders {
-		fmt.Fprintf(h, "%s:%s\n", strings.ToLower(k), strings.TrimSpace(v))
+	for _, k := range sortedSH {
+		fmt.Fprintf(h, "%s:%s\n", strings.ToLower(k), strings.TrimSpace(signedHeaders[k]))
 	}
 	fmt.Fprintf(h, "\n")
 	// End Canonical Headers
 
 	// Start Signed Headers
 	first = true
-	for k := range signedHeaders {
+	for k := range sortedSH {
 		if first {
-			fmt.Fprintf(h, "%s", url.QueryEscape(k))
+			fmt.Fprintf(h, "%s", url.QueryEscape(sortedSH[k]))
 			first = false
 		} else {
-			fmt.Fprintf(h, "%s", url.QueryEscape(";"+k))
+			fmt.Fprintf(h, "%s", url.QueryEscape(";"+sortedSH[k]))
 		}
 	}
 	fmt.Fprintf(h, "\n")
@@ -126,7 +139,8 @@ func (s3 *S3) GeneratePresignedURL(in PresignedInput) string {
 		[]byte("s3")),
 		[]byte("aws4_request"),
 	)
-	// sigKey gen verified using https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-other
+	// sigKey gen verified using
+	// https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-other
 	// (TODO: add a test using the same, consolidate with signKeys())
 
 	signedStrToSign := makeHMac(sigKey, stringToSign)
@@ -142,21 +156,20 @@ func (s3 *S3) GeneratePresignedURL(in PresignedInput) string {
 
 	// We don't need to have a sorted order here,
 	// but just to preserve tests.
-	for _, k := range keysQS {
+	for _, k := range sortedQS {
 		fmt.Fprintf(&b, "%s=%s&", url.QueryEscape(k), url.QueryEscape(queryString[k])) // HTTP Verb
 	}
 	fmt.Fprintf(&b, "%s=", "X-Amz-SignedHeaders")
 	first = true
-	for k := range signedHeaders {
+	for k := range sortedSH {
 		if first {
-			fmt.Fprintf(&b, "%s", url.QueryEscape(k))
+			fmt.Fprintf(&b, "%s", url.QueryEscape(sortedSH[k]))
 			first = false
 		} else {
-			fmt.Fprintf(&b, "%s", url.QueryEscape(";"+k))
+			fmt.Fprintf(&b, "%s", url.QueryEscape(";"+sortedSH[k]))
 		}
 	}
 	fmt.Fprintf(&b, "&X-Amz-Signature=%s", signature)
-	resultURL := b.String()
 
-	return resultURL
+	return b.String()
 }
